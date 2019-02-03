ded54795ade49e857926b0a69043480cea92a683
haydenlc3 <haydenlc3@gmail.com>
2019-02-03 15:25:49 -0600

    v1.0

---
+++ src/tictactoe/Board.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+/**
+ *
+ * @author HLC
+ */
+public class Board {
+    private Piece[][] board;
+    private int rows;
+    private int cols;
+    private int turn = 0;
+    private String winner;
+    
+    public Board(int rows, int cols) {
+        board = new Piece[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
+    }
+    
+    public void play() {
+        turn++;
+    }
+    
+    public int getTurn() {
+        return turn;
+    }
+    
+    public void addPiece(Piece piece, int row, int col) {
+        board[row][col] = piece;
+    }
+    
+    public boolean isWinner() {
+        for (int r = 0; r < rows; r++) { // check columns
+            int countX = 0;
+            int countO = 0;
+            
+            for (int c = 0; c < cols; c++) {
+                if (board[r][c] != null) {
+                    if (board[r][c].getType() == "X") {
+                        countX++;
+                    } else {
+                        countO++;
+                    }
+                }
+            }
+            
+            if (countX == cols) {
+                winner = "Player1";
+                return true;
+            } else if (countO == cols) {
+                winner = "Player2";
+                return true;
+            }
+        }
+        
+        for (int r = 0; r < rows; r++) { // check rows
+            int countX = 0;
+            int countO = 0;
+            
+            for (int c = 0; c < cols; c++) {
+                if (board[c][r] != null) {
+                    if (board[c][r].getType() == "X") {
+                        countX++;
+                    } else {
+                        countO++;
+                    }
+                }
+            }
+            
+            if (countX == rows) {
+                winner = "Player1";
+                return true;
+            } else if (countO == rows) {
+                winner = "Player2";
+                return true;
+            }
+        }
+        
+        int[] counts = new int[4];
+        
+        for (int a = 0; a < rows; a++) { // check diagonals
+            if (board[a][a] != null) {
+                if (board[a][a].getType() == "X") {
+                    counts[0]++;
+                } else {
+                    counts[1]++;
+                }
+            }
+            
+            if (board[a][cols-1-a] != null) {
+                if (board[a][cols-1-a].getType() == "X") {
+                    counts[2]++;
+                } else {
+                    counts[3]++;
+                }
+            }
+        }
+        
+        if (counts[0] == rows || counts[2] == rows) {
+            winner = "Player1";
+            return true;
+        } else if (counts[1] == rows || counts[3] == rows) {
+            winner = "Player2";
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean isTie() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (board[r][c] == null) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public String getWinner() {
+        return winner;
+    }
+    
+    public boolean notPiece(int row, int col) {
+        return board[row][col] == null;
+    }
+}
---
+++ src/tictactoe/GUI.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+
+/**
+ *
+ * @author HLC
+ */
+public class GUI extends JFrame{
+    private JFrame frame;
+    private JButton[] button;
+    private Board board;
+    private int rows;
+    private int cols;
+
+    public GUI(int r, int c) {
+        frame = new JFrame();
+        button = new JButton[r * c];
+        board = new Board(r, c);
+        rows = r;
+        cols = c;
+        
+        for (int i = 0; i < r * c; i++) {
+            button[i] = new JButton();
+            button[i].setFont(new Font("Monospace", Font.BOLD, 75));
+            button[i].addActionListener( new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    for (int i = 0; i < button.length; i++) {
+                        if (e.getSource().equals(button[i])) {
+                            if (board.notPiece(i%r, i/c)) {
+                                if (board.getTurn() % 2 == 0) {
+                                    board.addPiece(new Piece("X", i%r, i/c), i%r, i/c);
+                                    button[i].setText("X");
+                                    button[i].setForeground(Color.red);
+                                } else {
+                                    board.addPiece(new Piece("O", i%r, i/c), i%r, i/c);
+                                    button[i].setText("O");
+                                    button[i].setForeground(Color.GREEN);
+                                }
+                                
+                                if (board.isWinner()) {                                    
+                                    showConfirmation(board.getWinner());
+                                } else if (board.isTie()) {
+                                    showConfirmation("Nobody");
+                                } else {
+                                    board.play();
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            frame.add(button[i]);
+        }
+        
+        frame.setLayout(new GridLayout(r, c));
+        frame.setSize(r * 200 + 20, c * 200 + 20);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    
+    public void showConfirmation(String winner) {
+        int selectedOption = JOptionPane.showConfirmDialog(null, 
+        winner + " wins!\nWant to play again?", 
+        "Play Again", 
+        JOptionPane.YES_NO_OPTION); 
+
+        if (selectedOption == JOptionPane.YES_OPTION) {
+            board = new Board(rows, cols);
+            
+            for (int i = 0; i < button.length; i++) {
+                button[i].setText("");
+            }
+        } else if (selectedOption == JOptionPane.NO_OPTION) {
+            System.exit(0);
+        }   
+    }
+}
